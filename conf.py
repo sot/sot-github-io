79a1a486bb57395e4cb2a54486faf97d32ee1e97
Tom Aldcroft <taldcroft@gmail.com>
2025-01-27 16:32:22 -0500

    Update for pydata-sphinx-theme

--- conf.py
+++ conf.py
@@ -6,22 +6,45 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'sot.github.io'
-copyright = '2025, Tom Aldcroft'
+project = 'Ska'
+copyright = '2025, Smithsonian Astrophysical Observatory'
 author = 'Tom Aldcroft'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
+# Sphinx extension module names here, as strings. They can be extensions
+# coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = []
 
 templates_path = ['_templates']
+
+# Version information from the package.
+version = "1.0"
+release = version
+
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# -- Options for HTML output ---------------------------------------------------
+html_baseurl = "https://sot.github.io"
 
+# The theme to use for HTML and HTML Help pages.
+html_theme = 'pydata_sphinx_theme'
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+html_theme_options = {
+    "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/sot/sot-github-io",
+            "icon": "fab fa-github-square",
+        },
+    ],
+    "navbar_start": ["navbar-project-version"],
+    "navbar_end": ["theme-switcher", "navbar-icon-links"],
+    "secondary_sidebar_items": ["page-toc"],
+}
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+# No left sidebar
+html_sidebars = {
+  "**": []
+}
